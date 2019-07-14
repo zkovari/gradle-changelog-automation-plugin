0bea88c2590b1664a964dc378d6735ba9d9ad446
zkovari <zsolt@kovaridev.com>
2019-07-14 22:40:53 +0200

    Add fetchChangelogScript task
    Signed-off-by: zkovari <zsolt@kovaridev.com>

--- subprojects/changelog-automation-gradle-plugin/src/main/java/org/zkovari/changelog/gradle/api/plugins/ChangelogAutomationPlugin.java
+++ subprojects/changelog-automation-gradle-plugin/src/main/java/org/zkovari/changelog/gradle/api/plugins/ChangelogAutomationPlugin.java
@@ -1,10 +1,25 @@
 package org.zkovari.changelog.gradle.api.plugins;
 
+import java.io.File;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.tasks.OutputDirectory;
+import org.zkovari.changelog.gradle.api.tasks.FetchChangelogScript;
 import org.zkovari.changelog.gradle.api.tasks.ProcessChangelogEntries;
 
 public class ChangelogAutomationPlugin implements Plugin<Project> {
+
+    private File outputDirectory;
+
+    @OutputDirectory
+    public File getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    public void setOutputDirectory(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
 
     @Override
     public void apply(Project project) {
@@ -12,6 +27,15 @@ public class ChangelogAutomationPlugin implements Plugin<Project> {
                 ProcessChangelogEntries.class);
         processTask.setInputDirectory(project.file("changelogs/unreleased"));
         processTask.setOutputfile(project.file("CHANGELOG.md"));
+        processTask.setGroup("Changelog");
+        processTask.setDescription("Processes changelog entries and combine them into CHANGELOG.md");
+
+        FetchChangelogScript fetchChangelogScriptTask = project.getTasks().create("fetchChangelogScript",
+                FetchChangelogScript.class);
+        fetchChangelogScriptTask.setOutputDirectory(project.getRootProject().file("scripts"));
+        fetchChangelogScriptTask.getOutputs().upToDateWhen(t -> false);
+        fetchChangelogScriptTask.setGroup("Changelog");
+        fetchChangelogScriptTask.setDescription("Fetches the changelog.sh script into your project");
     }
 
 }
