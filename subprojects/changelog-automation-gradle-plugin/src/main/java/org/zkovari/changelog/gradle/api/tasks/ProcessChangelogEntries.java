fab95857b329331348291e1a15a14c70f759e3c7
zkovari <zsolt@kovaridev.com>
2019-07-14 08:35:37 +0200

    Remove custom writer class
    Signed-off-by: zkovari <zsolt@kovaridev.com>

--- subprojects/changelog-automation-gradle-plugin/src/main/java/org/zkovari/changelog/gradle/api/tasks/ProcessChangelogEntries.java
+++ subprojects/changelog-automation-gradle-plugin/src/main/java/org/zkovari/changelog/gradle/api/tasks/ProcessChangelogEntries.java
@@ -2,18 +2,19 @@ package org.zkovari.changelog.gradle.api.tasks;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
 import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.zkovari.changelog.core.generator.ChangelogGenerator;
 import org.zkovari.changelog.core.parser.ChangelogEntriesParser;
 import org.zkovari.changelog.core.parser.ChangelogParserException;
 import org.zkovari.changelog.core.processor.ChangelogEntriesProcessor;
 import org.zkovari.changelog.core.processor.InvalidChangelogEntryException;
-import org.zkovari.changelog.core.writer.ChangelogWriter;
 import org.zkovari.changelog.domain.ChangelogEntry;
 import org.zkovari.changelog.domain.Release;
 
@@ -24,63 +25,63 @@ public class ProcessChangelogEntries extends DefaultTask {
 
     @InputDirectory
     public File getInputDirectory() {
-	return inputDirectory;
+        return inputDirectory;
     }
 
     public void setInputDirectory(File inputDirectory) {
-	this.inputDirectory = inputDirectory;
+        this.inputDirectory = inputDirectory;
     }
 
+    @OutputFile
     public File getOutputfile() {
-	return outputfile;
+        return outputfile;
     }
 
     public void setOutputfile(File outputfile) {
-	this.outputfile = outputfile;
+        this.outputfile = outputfile;
     }
 
     @TaskAction
     public void process() {
-	List<ChangelogEntry> entries = getEntries();
-	Release release = processEntries(entries);
-	String newChangelogContent = generate(release);
-	write(newChangelogContent);
-    }
-
-    private void write(String newChangelogContent) {
-	ChangelogWriter writer = new ChangelogWriter();
-	try {
-	    writer.write(outputfile, newChangelogContent);
-	} catch (IOException ex) {
-	    throw new GradleException(ex.getMessage(), ex);
-	}
-    }
-
-    private String generate(Release release) {
-	ChangelogGenerator generator = new ChangelogGenerator();
-	return generator.generate("", release);
-    }
-
-    private Release processEntries(List<ChangelogEntry> entries) {
-	ChangelogEntriesProcessor processor = new ChangelogEntriesProcessor();
-	try {
-	    return processor.processChangelogs(entries, getProject().getVersion().toString());
-	} catch (InvalidChangelogEntryException ex) {
-	    throw new GradleException(ex.getMessage(), ex);
-	}
+        List<ChangelogEntry> entries = getEntries();
+        Release release = processEntries(entries);
+        String newChangelogContent = generate(release);
+        write(newChangelogContent);
     }
 
     private List<ChangelogEntry> getEntries() {
-	ChangelogEntriesParser parser = new ChangelogEntriesParser();
-	List<ChangelogEntry> entries;
-	try {
-	    entries = parser.parse(inputDirectory);
-	} catch (ChangelogParserException ex) {
-	    throw new GradleException(
-		    "Could not parse new changelog entries under " + inputDirectory + ". Reason: " + ex.getMessage(),
-		    ex);
-	}
-	return entries;
+        ChangelogEntriesParser parser = new ChangelogEntriesParser();
+        List<ChangelogEntry> entries;
+        try {
+            entries = parser.parse(inputDirectory);
+        } catch (ChangelogParserException ex) {
+            throw new GradleException(
+                    "Could not parse new changelog entries under " + inputDirectory + ". Reason: " + ex.getMessage(),
+                    ex);
+        }
+        return entries;
+    }
+
+    private Release processEntries(List<ChangelogEntry> entries) {
+        ChangelogEntriesProcessor processor = new ChangelogEntriesProcessor();
+        try {
+            return processor.processChangelogs(entries, getProject().getVersion().toString());
+        } catch (InvalidChangelogEntryException ex) {
+            throw new GradleException(ex.getMessage(), ex);
+        }
+    }
+
+    private String generate(Release release) {
+        ChangelogGenerator generator = new ChangelogGenerator();
+        return generator.generate("", release);
+    }
+
+    private void write(String newChangelogContent) {
+        try {
+            Files.write(outputfile.toPath(), newChangelogContent.getBytes());
+        } catch (IOException ex) {
+            throw new GradleException(ex.getMessage(), ex);
+        }
     }
 
 }
