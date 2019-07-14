9da775437600a77e855d9b03e000828da85f1f60
zkovari <zsolt@kovaridev.com>
2019-07-14 22:40:53 +0200

    Add fetchChangelogScript task
    Signed-off-by: zkovari <zsolt@kovaridev.com>

---
+++ subprojects/changelog-automation-gradle-plugin/src/main/java/org/zkovari/changelog/gradle/api/tasks/FetchChangelogScript.java
@@ -0,0 +1,38 @@
+package org.zkovari.changelog.gradle.api.tasks;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.TaskAction;
+
+public class FetchChangelogScript extends DefaultTask {
+
+    private File outputDirectory;
+
+    @OutputDirectory
+    public File getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    public void setOutputDirectory(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    @TaskAction
+    public void fetch() {
+        InputStream in = getClass().getClassLoader().getResourceAsStream("changelog.sh");
+
+        try {
+            Files.copy(in, outputDirectory.toPath().resolve("changelog.sh"), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            throw new GradleException("Could not copy changelog.sh file into: " + outputDirectory, ex);
+        }
+    }
+
+}
--- subprojects/changelog-automation-gradle-plugin/src/test/groovy/org/zkovari/changelog/gradle/api/plugins/ChangelogAutomationPluginFunctionalTest.groovy
+++ subprojects/changelog-automation-gradle-plugin/src/test/groovy/org/zkovari/changelog/gradle/api/plugins/ChangelogAutomationPluginFunctionalTest.groovy
@@ -18,14 +18,26 @@ class ChangelogAutomationPluginFunctionalTest extends Specification {
     File propertiesFile
     File buildFile
 
+    File changelogEntry1
+
     def setup() {
         File testKitGradlePropertiesResource = new File(
                 getClass().getClassLoader().getResource("testkit-gradle.properties").toURI())
         testKitGradleProperties = new String(Files.readAllBytes(testKitGradlePropertiesResource.toPath()))
-        testProjectDir.newFolder('changelogs', 'unreleased')
+        def changlogEntriesDir = testProjectDir.newFolder('changelogs', 'unreleased')
         settingsFile = testProjectDir.newFile('settings.gradle')
         propertiesFile = testProjectDir.newFile('gradle.properties')
         buildFile = testProjectDir.newFile('build.gradle')
+        changelogEntry1 = new File(changlogEntriesDir, "entry1.yml")
+
+        settingsFile << "rootProject.name = 'test-project'"
+        propertiesFile << testKitGradleProperties
+        buildFile << """
+            plugins {
+                id 'org.zkovari.changelog'
+            }
+            version='1.0.0'
+        """
     }
 
     BuildResult getRunner(String command) {
@@ -37,19 +49,53 @@ class ChangelogAutomationPluginFunctionalTest extends Specification {
     }
 
     def "run processChangelogEntries when input folder is empty"() {
+        when:
+        BuildResult result = getRunner('processChangelogEntries')
+
+        then:
+        assert result.task(":processChangelogEntries").outcome == TaskOutcome.SUCCESS
+    }
+
+    def "run processChangelogEntries with changelog entry "(){
         given:
-        settingsFile << "rootProject.name = 'test-project'"
-        propertiesFile << testKitGradleProperties
-        buildFile << """
-            plugins {
-                id 'org.zkovari.changelog'
-            }
+        changelogEntry1 << """
+            title: "Title"
+            type: "added"
+            reference: ""
+            author: ""
         """
 
         when:
         BuildResult result = getRunner('processChangelogEntries')
 
         then:
-        result.task(":processChangelogEntries").outcome == TaskOutcome.SUCCESS
+        assert result.task(":processChangelogEntries").outcome == TaskOutcome.SUCCESS
+        def changelog = new File(testProjectDir.root, "CHANGELOG.md")
+        assert changelog.exists()
+        assert changelog.text.contains("Title") && changelog.text.contains("Added") && changelog.text.contains("1.0.0")
+        // TODO original entry should be removed
+        //assert !changelogEntry1.exists()
+    }
+
+    def "run fetchChangelogScript"() {
+        when:
+        BuildResult result = getRunner('fetchChangelogScript')
+
+        then:
+        assert result.task(":fetchChangelogScript").outcome == TaskOutcome.SUCCESS
+        def changelogScript = new File(testProjectDir.root, "scripts/changelog.sh")
+        assert changelogScript.exists() && changelogScript.canExecute()
+    }
+
+    def "run fetchChangelogScript twice to update script"() {
+        when:
+        BuildResult result1 = getRunner('fetchChangelogScript')
+        BuildResult result2 = getRunner('fetchChangelogScript')
+
+        then:
+        assert result1.task(":fetchChangelogScript").outcome == TaskOutcome.SUCCESS
+        assert result2.task(":fetchChangelogScript").outcome == TaskOutcome.SUCCESS
+        def changelogScript = new File(testProjectDir.root, "scripts/changelog.sh")
+        assert changelogScript.exists() && changelogScript.canExecute()
     }
 }
