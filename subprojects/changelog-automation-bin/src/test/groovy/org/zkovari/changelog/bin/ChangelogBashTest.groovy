3f80e59b628ee80895ed1f43f2c1c22634bb6140
zkovari <zsolt@kovaridev.com>
2019-07-14 17:09:53 +0200

    Finish script
    Signed-off-by: zkovari <zsolt@kovaridev.com>

--- subprojects/changelog-automation-bin/src/test/groovy/org/zkovari/changelog/bin/ChangelogBashTest.groovy
+++ subprojects/changelog-automation-bin/src/test/groovy/org/zkovari/changelog/bin/ChangelogBashTest.groovy
@@ -12,6 +12,7 @@ class ChangelogBashTest extends Specification {
     @Rule final TemporaryFolder testDir = new TemporaryFolder()
 
     private File changelog
+    private File unreleasedDir
     private StringBuilder standardError
     private StringBuilder standardOutput
 
@@ -20,6 +21,7 @@ class ChangelogBashTest extends Specification {
         InputStream inputStream = changelogUrl.openStream()
         changelog = testDir.newFile(CHANGELOG_SH)
         changelog << inputStream
+        unreleasedDir = new File(testDir.root, "changelogs/unreleased")
 
         standardOutput = new StringBuilder()
         standardError = new StringBuilder()
@@ -30,9 +32,26 @@ class ChangelogBashTest extends Specification {
     }
 
     private run(String args = "") {
-        def proc = "bash $testDir.root/changelog.sh $args".execute()
+        def proc = "bash ${testDir.root}/changelog.sh $args".execute(null, testDir.root)
         proc.consumeProcessOutput(standardOutput, standardError)
         proc.waitForOrKill(1000)
+    }
+
+    private String getGeneratedChangelogContent(String expectedFileName=null) {
+        assert standardOutput.toString().contains("New changelog was generated to:")
+        def generatedEntries = (unreleasedDir.list() as List)
+        assert unreleasedDir.exists() && unreleasedDir.directory && !generatedEntries.empty
+        assert generatedEntries.size == 1
+        File changelogFile = new File(unreleasedDir, generatedEntries.get(0))
+        if (expectedFileName) {
+            assert changelogFile.name == expectedFileName
+        } else {
+            // e.g. for 2019-07-14_15-19-01_changelog_entry.yml:
+            //                              2019   - 07     -  14    _   15   -    19  -    01  _changelog_entry.yml
+            assert changelogFile.name =~ "^[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}_changelog_entry\\.yml"
+        }
+        def actualChangelogContent = changelogFile.text
+        return actualChangelogContent
     }
 
     def "show help if run with -h"() {
@@ -41,6 +60,7 @@ class ChangelogBashTest extends Specification {
 
         then:
         assert standardOutput.toString().contains("Usage: changelog.sh [OPTION]... --type [TYPE] [TITLE]")
+        assert !unreleasedDir.exists()
     }
 
     def "show help if run with --help"() {
@@ -49,6 +69,7 @@ class ChangelogBashTest extends Specification {
 
         then:
         assert standardOutput.toString().contains("Usage: changelog.sh [OPTION]... --type [TYPE] [TITLE]")
+        assert !unreleasedDir.exists()
     }
 
     def "show help if run without arguments"() {
@@ -56,8 +77,9 @@ class ChangelogBashTest extends Specification {
         run()
 
         then:
-        assert standardOutput.toString().contains("Title and type must be specified")
+        assert standardOutput.toString().contains("Title must be specified")
         assert standardOutput.toString().contains("Usage: changelog.sh [OPTION]... --type [TYPE] [TITLE]")
+        assert !unreleasedDir.exists()
     }
 
     def "show help if run without --type"() {
@@ -65,17 +87,19 @@ class ChangelogBashTest extends Specification {
         run("title")
 
         then:
-        assert standardOutput.toString().contains("Title and type must be specified")
+        assert standardOutput.toString().contains("Type must be specified")
         assert standardOutput.toString().contains("Usage: changelog.sh [OPTION]... --type [TYPE] [TITLE]")
+        assert !unreleasedDir.exists()
     }
 
     def "show help if run without title"() {
         when:
-        run("--type added")
+        run("--type added --dry-run --reference 123")
 
         then:
-        assert standardOutput.toString().contains("Title and type must be specified")
+        assert standardOutput.toString().contains("Title must be specified")
         assert standardOutput.toString().contains("Usage: changelog.sh [OPTION]... --type [TYPE] [TITLE]")
+        assert !unreleasedDir.exists()
     }
 
     def "show version if run with -v"() {
@@ -84,6 +108,7 @@ class ChangelogBashTest extends Specification {
 
         then:
         assert standardOutput.toString().contains("Version:")
+        assert !unreleasedDir.exists()
     }
 
     def "show version if run with --version"() {
@@ -92,5 +117,128 @@ class ChangelogBashTest extends Specification {
 
         then:
         assert standardOutput.toString().contains("Version:")
+        assert !unreleasedDir.exists()
+    }
+
+    def "abort if --type is invalid"() {
+        when:
+        run("--type other 'title'")
+
+        then:
+        assert standardOutput.toString().contains("Invalid value was specified for --type: other")
+        assert !unreleasedDir.exists()
+    }
+
+    def "abort if -t is invalid"() {
+        when:
+        run("-t other 'title'")
+
+        then:
+        assert standardOutput.toString().contains("Invalid value was specified for --type: other")
+        assert !unreleasedDir.exists()
+    }
+
+    def "generate added changelog"() {
+        when:
+        run("--type added 'Title message'")
+
+        then:
+        def changelogContent = getGeneratedChangelogContent()
+        assert changelogContent.contains('# Auto-generated by changelog.sh script. Version:')
+        assert changelogContent.contains('title: "Title message"')
+        assert changelogContent.contains('reference: ""')
+        assert changelogContent.contains('author: ""')
+        assert changelogContent.contains('type: "added"')
+    }
+
+    def "generate changed changelog"() {
+        when:
+        run("--type changed 'Title message'")
+
+        then:
+        def changelogContent = getGeneratedChangelogContent()
+        assert changelogContent.contains('# Auto-generated by changelog.sh script. Version:')
+        assert changelogContent.contains('title: "Title message"')
+        assert changelogContent.contains('reference: ""')
+        assert changelogContent.contains('author: ""')
+        assert changelogContent.contains('type: "changed"')
+    }
+
+    def "generate deprecated changelog"() {
+        when:
+        run("--type deprecated 'Title message'")
+
+        then:
+        def changelogContent = getGeneratedChangelogContent()
+        assert changelogContent.contains('# Auto-generated by changelog.sh script. Version:')
+        assert changelogContent.contains('title: "Title message"')
+        assert changelogContent.contains('reference: ""')
+        assert changelogContent.contains('author: ""')
+        assert changelogContent.contains('type: "deprecated"')
+    }
+
+    def "generate fixed changelog"() {
+        when:
+        run("--type fixed 'Title message'")
+
+        then:
+        def changelogContent = getGeneratedChangelogContent()
+        assert changelogContent.contains('# Auto-generated by changelog.sh script. Version:')
+        assert changelogContent.contains('title: "Title message"')
+        assert changelogContent.contains('reference: ""')
+        assert changelogContent.contains('author: ""')
+        assert changelogContent.contains('type: "fixed"')
+    }
+
+    def "generate removed changelog"() {
+        when:
+        run("--type removed 'Title message'")
+
+        then:
+        def changelogContent = getGeneratedChangelogContent()
+        assert changelogContent.contains('# Auto-generated by changelog.sh script. Version:')
+        assert changelogContent.contains('title: "Title message"')
+        assert changelogContent.contains('reference: ""')
+        assert changelogContent.contains('author: ""')
+        assert changelogContent.contains('type: "removed"')
+    }
+
+    def "generate security changelog"() {
+        when:
+        run("--type security 'Title message'")
+
+        then:
+        def changelogContent = getGeneratedChangelogContent()
+        assert changelogContent.contains('# Auto-generated by changelog.sh script. Version:')
+        assert changelogContent.contains('title: "Title message"')
+        assert changelogContent.contains('reference: ""')
+        assert changelogContent.contains('author: ""')
+        assert changelogContent.contains('type: "security"')
+    }
+
+    def "generate changelog with reference -r"() {
+        when:
+        run("--type added -r 123 'Title message'")
+
+        then:
+        def changelogContent = getGeneratedChangelogContent()
+        assert changelogContent.contains('# Auto-generated by changelog.sh script. Version:')
+        assert changelogContent.contains('title: "Title message"')
+        assert changelogContent.contains('reference: "123"')
+        assert changelogContent.contains('author: ""')
+        assert changelogContent.contains('type: "added"')
+    }
+
+    def "generate changelog with reference --reference"() {
+        when:
+        run("--type added --reference 123 'Title message'")
+
+        then:
+        def changelogContent = getGeneratedChangelogContent()
+        assert changelogContent.contains('# Auto-generated by changelog.sh script. Version:')
+        assert changelogContent.contains('title: "Title message"')
+        assert changelogContent.contains('reference: "123"')
+        assert changelogContent.contains('author: ""')
+        assert changelogContent.contains('type: "added"')
     }
 }
