990741bf4668b8c1967cc057446438f0e5091445
zkovari <zsolt@kovaridev.com>
2019-07-15 19:57:51 +0200

    Add exec permission to fetched changelog script
    Signed-off-by: zkovari <zsolt@kovaridev.com>

--- subprojects/changelog-automation-gradle-plugin/src/main/java/org/zkovari/changelog/gradle/api/tasks/FetchChangelogScript.java
+++ subprojects/changelog-automation-gradle-plugin/src/main/java/org/zkovari/changelog/gradle/api/tasks/FetchChangelogScript.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
@@ -28,10 +31,21 @@ public class FetchChangelogScript extends DefaultTask {
     public void fetch() {
         InputStream in = getClass().getClassLoader().getResourceAsStream("changelog.sh");
 
+        Path outputChangelog = outputDirectory.toPath().resolve("changelog.sh");
         try {
-            Files.copy(in, outputDirectory.toPath().resolve("changelog.sh"), StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(in, outputChangelog, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException ex) {
             throw new GradleException("Could not copy changelog.sh file into: " + outputDirectory, ex);
+        }
+
+        HashSet<PosixFilePermission> permissions = new HashSet<>();
+        permissions.add(PosixFilePermission.OWNER_EXECUTE);
+        permissions.add(PosixFilePermission.GROUP_EXECUTE);
+        permissions.add(PosixFilePermission.OTHERS_EXECUTE);
+        try {
+            Files.setPosixFilePermissions(outputChangelog, permissions);
+        } catch (IOException e) {
+            getLogger().warn("Could not set executable permissions to file {}", outputChangelog);
         }
     }
 
