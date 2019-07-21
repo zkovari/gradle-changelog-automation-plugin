038d00dd973b6a83547fd128910c9a5f6d6abd26
Zsolt Kovari <zsolt@kovaridev.com>
2019-07-21 12:20:11 +0200

    Add postDeploy stage for automatic tagging, changelog generation, and version bump (#12)

---
+++ scripts/create_tag.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+version=$(grep version= "$dir/../gradle.properties" | sed s/version=//)
+
+git tag "v$version" -a -m "Create tag v$version"
