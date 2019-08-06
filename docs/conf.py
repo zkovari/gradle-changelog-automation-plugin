44af20bb6dc44ba8c0877871392eea761e20b7a8
zkovari <zsolt@kovaridev.com>
2019-08-06 20:37:30 +0200

    #35 Accept shortened values for types in changelog.sh

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Zsolt Kovari'
 author = 'Zsolt Kovari'
 
 # The short X.Y version
-version = '0.2.2'
+version = '0.3.0'
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
