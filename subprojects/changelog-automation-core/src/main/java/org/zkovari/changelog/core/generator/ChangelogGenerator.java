bff092da2bdf210348050d22809e6e9626ae1084
zkovari <zsolt@kovaridev.com>
2020-03-17 21:28:21 +0100

    Format ChangelogGenerator.java

--- subprojects/changelog-automation-core/src/main/java/org/zkovari/changelog/core/generator/ChangelogGenerator.java
+++ subprojects/changelog-automation-core/src/main/java/org/zkovari/changelog/core/generator/ChangelogGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2019 Zsolt Kovari
+ * Copyright 2019-2020 Zsolt Kovari
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
@@ -29,49 +29,50 @@ public class ChangelogGenerator {
     private ReleaseEntryGenerator releaseEntryGenerator;
 
     static {
-	VERSION_PATTERN = Pattern.compile(VERSION_REGEX);
+        VERSION_PATTERN = Pattern.compile(VERSION_REGEX);
     }
 
     public String generate(String currentChangelogContent, Release release) {
-	String newChangelog = currentChangelogContent;
-	String newReleaseEntry = getReleaseEntryGenerator().generate(release);
+        String newChangelog = currentChangelogContent;
+        String newReleaseEntry = getReleaseEntryGenerator().generate(release);
 
-	Matcher matcher = VERSION_PATTERN.matcher(currentChangelogContent);
-	boolean foundLastVersion = matcher.find();
-	if (foundLastVersion) {
-	    String previousVersion = matcher.group();
-	    newChangelog = matcher.replaceFirst(newReleaseEntry + previousVersion);
-	} else {
-	    if (currentChangelogContent.isEmpty()) {
-		newChangelog = getChangelogHeader();
-	    }
-	    newChangelog = newChangelog + NEW_LINE + newReleaseEntry;
-	}
+        Matcher matcher = VERSION_PATTERN.matcher(currentChangelogContent);
+        boolean foundLastVersion = matcher.find();
+        if (foundLastVersion) {
+            String previousVersion = matcher.group();
+            newChangelog = matcher.replaceFirst(newReleaseEntry + previousVersion);
+        } else {
+            if (currentChangelogContent.isEmpty()) {
+                newChangelog = getChangelogHeader();
+            }
+            newChangelog = newChangelog + NEW_LINE + newReleaseEntry;
+        }
 
-	return newChangelog;
+        return newChangelog;
     }
 
     private String getChangelogHeader() {
-	// @formatter:off
-	return new StringBuilder("# Changelog").append(NEW_LINE)
-		.append("All notable changes to this project will be documented in this file.").append(NEW_LINE)
-		.append(NEW_LINE)
-		.append("The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),").append(NEW_LINE)
-		.append("and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).").append(NEW_LINE)
-		.toString();
-	// @formatter:on
+        // @formatter:off
+        return new StringBuilder("# Changelog").append(NEW_LINE)
+                .append("All notable changes to this project will be documented in this file.").append(NEW_LINE)
+                .append(NEW_LINE)
+                .append("The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),")
+                .append(NEW_LINE)
+                .append("and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).")
+                .append(NEW_LINE).toString();
+        // @formatter:on
     }
 
     public ReleaseEntryGenerator getReleaseEntryGenerator() {
-	if (releaseEntryGenerator == null) {
-	    releaseEntryGenerator = new ReleaseEntryGenerator();
-	}
-	return releaseEntryGenerator;
+        if (releaseEntryGenerator == null) {
+            releaseEntryGenerator = new ReleaseEntryGenerator();
+        }
+        return releaseEntryGenerator;
     }
 
     // package level for testing
     void setReleaseEntryGenerator(ReleaseEntryGenerator releaseEntryGenerator) {
-	this.releaseEntryGenerator = releaseEntryGenerator;
+        this.releaseEntryGenerator = releaseEntryGenerator;
     }
 
 }
